2b6110f4876d61cd3ff14128e514f1900ac555d1
helkyar <javierpalaciosbotejara@gmail.com>
2024-10-22 10:34:29 +0200

    refactor quiz component

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,54 +1,46 @@
 import { useQuestionsStore } from '../store/useQuestionsStore'
-import { Question } from '../types'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { gradientDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import { Question } from '../types'
 
+function getOptionClassName(quiz: Question, index: number) {
+  const incorrect =
+    quiz.userSelectedAnswer === index && !quiz.isCorrectUserAnswer
+  const correct =
+    quiz.userSelectedAnswer != null && quiz.correctAnswer === index
+  return correct ? 'correct' : incorrect ? 'incorrect' : ''
+}
 function Quiz() {
   const currentQuestion = useQuestionsStore((state) => state.currentQuestion)
   const questions = useQuestionsStore((state) => state.questions)
   const selectAnswer = useQuestionsStore((state) => state.selectAnswer)
+  const quiz = questions[currentQuestion]
 
   return (
     <>
       <section className="questions">
-        {questions.map((question: Question, i: number) => (
-          <div key={question.id}>
-            {i === currentQuestion && (
-              <div>
-                <p className="title">{question.question}</p>
-                <SyntaxHighlighter
-                  className="code"
-                  language="javascript"
-                  style={gradientDark}
-                >
-                  {question.code}
-                </SyntaxHighlighter>
-                <div className="options">
-                  {question.answers.map((answer, i) => (
-                    <button
-                      key={i}
-                      disabled={question.userSelectedAnswer != null}
-                      className={`${
-                        question.userSelectedAnswer === i &&
-                        !question.isCorrectUserAnswer
-                          ? 'incorrect'
-                          : ''
-                      } ${
-                        question.userSelectedAnswer !== undefined &&
-                        question.correctAnswer === i
-                          ? 'correct'
-                          : ''
-                      } `}
-                      onClick={() => selectAnswer(question.id, i)}
-                    >
-                      {answer}
-                    </button>
-                  ))}
-                </div>
-              </div>
-            )}
+        <div>
+          <p className="title">{quiz.question}</p>
+          <SyntaxHighlighter
+            className="code"
+            language="javascript"
+            style={gradientDark}
+          >
+            {quiz.code}
+          </SyntaxHighlighter>
+          <div className="options">
+            {quiz.answers.map((answer, i) => (
+              <button
+                key={i}
+                disabled={quiz.userSelectedAnswer != null}
+                className={getOptionClassName(quiz, i)}
+                onClick={() => selectAnswer(quiz.id, i)}
+              >
+                {answer}
+              </button>
+            ))}
           </div>
-        ))}
+        </div>
       </section>
     </>
   )
